3ad1948e05cebb3811b11ed5aee7a7e322f28b3c
mattia <mattia.porco981@gmail.com>
2021-12-21 20:22:28 +0100

    First stage

--- src/main.py
+++ src/main.py
@@ -1,13 +1,28 @@
 import matplotlib.pyplot as plt
+import helpers as helpers
 
-def blur_datasource():
-    A = pic = plt.imread('./datasource/one.png') 
-    plt.imshow(A, cmap='gray') 
+def blur_datasource(image_path, sigma, ker_len):
+    # Open the image as a matrix and show it
+    pic = plt.imread(image_path) 
+    pic = pic[:, : , 0]
+
+    # Get the kernel
+    ker = helpers.gaussian_kernel(ker_len, sigma)
+
+    # Appply Fourier
+    K = helpers.psf_fft(ker, ker_len, (512,512))
+
+    # Blur and show
+    blurred = helpers.A(pic, K)
+    plt.imshow(blurred, cmap='gray') 
     plt.show()
 
 
 def main():
-    blur_datasource()
+    ds1 = './datasource/one.png'
+    blur_datasource(ds1, 0.5, 5)
+    blur_datasource(ds1, 1, 7)
+    blur_datasource(ds1, 1.3, 9)
 
 if __name__ == "__main__":
     main()
