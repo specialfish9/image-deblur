eeb89eda05bb28cfec49cb533a7ddcdd17dc3159
mattia <mattia.porco981@gmail.com>
2021-12-27 21:52:25 +0100

    Noise

--- src/main.py
+++ src/main.py
@@ -2,30 +2,35 @@ import matplotlib.pyplot as plt
 import helpers as helpers
 import scipy.optimize as opt
 import numpy as np
+from skimage import data, metrics
 
 def lstsq(x, a, b):
-    return (1/2) * np.linalg.norm(np.dot(a, x) - b)**2
+    return (1/2) * np.linalg.norm(np.dot(a, x) - b) ** 2
 
 def grad(x, a, b):
-    return np.dot(a.T, np.dot(a, x) - b)
-
+    return np.dot(a.T, np.dot(a,x)) - np.dot(a.T, b)
 
 def blur_datasource(image_path, sigma, ker_len):
     # Open the image as a matrix and show it
     picture = plt.imread(image_path) 
     picture = picture[:, : , 0]
+    #picture = data.camera().astype(np.float64) / 255.0 
     n, m = picture.shape
 
     # Get the kernel
     kernel = helpers.gaussian_kernel(ker_len, sigma)
 
-    # Appply Fourier
+    # Apply Fourier
     k = np.real(helpers.psf_fft(kernel, ker_len, (n, m)))
 
-    # Blur and show
-    blurred = helpers.A(picture, k)
-    plt.imshow(blurred, cmap='gray') 
-    plt.show()
+    # Generate noise
+    s = 0.004
+    noise = np.random.normal(size=picture.shape) * s
+    
+    # Blur, add noise and show
+    blurred = helpers.A(picture, k) + noise
+    # plt.imshow(blurred, cmap='gray') 
+    # plt.show()
 
     # Reshape
     picture_vec = picture.reshape(n * m)
@@ -37,21 +42,22 @@ def blur_datasource(image_path, sigma, ker_len):
 
     x0 = np.zeros(n * m)
 
-    minimum = opt.minimize(f, x0, jac=grad_f, method='CG')
+    minimum = opt.minimize(f, x0, method='CG')
+    deblurred = minimum.x.reshape((n, m))
 
-    plt.imshow(minimum.x.reshape((n, m)), cmap='gray') 
+    plt.imshow(deblurred, cmap='gray') 
     plt.show()
 
     return blurred
 
 
 def main():
-    ds1 = './datasource/one.png'
+    ds1 = './datasource/e1.png'
 
     # TODO: add gaussian rumor between ]0; 0,05]
-    b1_ds1 = blur_datasource(ds1, 0.5, 5)
+    # b1_ds1 = blur_datasource(ds1, 0.5, 5)
     # b2_ds1 = blur_datasource(ds1, 1, 7)
-    # b3_ds1 = blur_datasource(ds1, 1.3, 9)
+    b3_ds1 = blur_datasource(ds1, 1.3, 9)
     
     # Add here calls to phase2
 
